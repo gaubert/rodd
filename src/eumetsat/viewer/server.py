884347e7687df2d3451fd9c2319df4991be1df37
Guillaume Aubert <guillaume.aubert@gmail.com>
2010-08-26 15:22:15 +0000

    add flickr dotted lines

--- src/eumetsat/viewer/server.py
+++ src/eumetsat/viewer/server.py
@@ -28,6 +28,39 @@ def view_table(a_table_name):
     except Exception, the_exc:
         return the_exc
 
+def view_request_results(a_request_name, a_sql_req):
+    
+    """
+       Return the content of the sql request provided its name
+    """
+    try:
+        conn = connections.DatabaseConnector("mysql://rodd:ddor@tclxs30/RODD")
+    
+        conn.connect()
+        
+        # get metadata to get the columns names
+        #metadata = conn.getTableMetadata(a_table_name)
+        
+        #col_names = [ dic["name"] for dic in metadata ]
+    
+        result = conn.execute(a_sql_req)
+        
+        cols = result.fetchall()
+        
+        #return "dir(cols[0]) =%s\n" %( dir(result) )
+        #return "dir(cols[0]) =%s\n" %( cols[0].keys() )
+        
+        cols_names = cols[0].keys()
+        
+        output = template('generic_table', rows = cols, heads = cols_names, table= a_request_name)
+        
+        print("got output\n")
+    
+        return output
+    
+    except Exception, the_exc:
+        return the_exc
+
 @route('/format_type')
 def view_format_type():
     """ 
@@ -62,8 +95,15 @@ def view_products_2_servdirs():
     """ 
        Viewer for products_2_servdirs 
     """
-    return view_table("products_2_servdirs")
+    #return view_table("products_2_servdirs")
+    return view_request_results("products2service", "select products.title as 'Product Name', service_dirs.name as 'Service Dir Name', products.roddID as 'roddID', service_dirs.servID as 'servID' from products, service_dirs, products_2_servdirs as p2s where p2s.roddID=products.roddID and p2s.servID=service_dirs.servID")
 
+@route('/tbl_prod2servdirs')
+def view_tbl_prod2servdirs():
+    """ 
+       Viewer table prod2servdirs
+    """
+    return view_table("products_2_servdirs")
 
 #route static files like (css, js, ...)
 @route('/media/:filename#.*#')
@@ -81,4 +121,6 @@ debug(True)
 
 #activate reloader for debugging purposes
 run(reloader=True, host='localhost', port=8080)
+
+
 
