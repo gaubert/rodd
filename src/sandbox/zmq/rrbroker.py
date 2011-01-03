ad6bc63ccfde904a986b647529ffd93c6bfa84f9
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-01-03 16:11:52 +0000

    finished rrbroker

--- src/sandbox/zmq/rrbroker.py
+++ src/sandbox/zmq/rrbroker.py
@@ -7,59 +7,7 @@
 """
 import zmq
 
-int main (int argc, char *argv[])
-{
-    // Prepare our context and sockets
-    void *context = zmq_init (1);
-    void *frontend = zmq_socket (context, ZMQ_XREP);
-    void *backend = zmq_socket (context, ZMQ_XREQ);
-    zmq_bind (frontend, "tcp://*:5559");
-    zmq_bind (backend, "tcp://*:5560");
 
-    // Initialize poll set
-    zmq_pollitem_t items [] = {
-        { frontend, 0, ZMQ_POLLIN, 0 },
-        { backend, 0, ZMQ_POLLIN, 0 }
-    };
-    // Switch messages between sockets
-    while (1) {
-        zmq_msg_t message;
-        int64_t more; // Multipart detection
-
-        zmq_poll (items, 2, -1);
-        if (items [0].revents & ZMQ_POLLIN) {
-            while (1) {
-                // Process all parts of the message
-                zmq_msg_init (&message);
-                zmq_recv (frontend, &message, 0);
-                size_t more_size = sizeof (more);
-                zmq_getsockopt (frontend, ZMQ_RCVMORE, &more, &more_size);
-                zmq_send (backend, &message, more? ZMQ_SNDMORE: 0);
-                zmq_msg_close (&message);
-                if (!more)
-                    break; // Last message part
-            }
-        }
-        if (items [1].revents & ZMQ_POLLIN) {
-            while (1) {
-                // Process all parts of the message
-                zmq_msg_init (&message);
-                zmq_recv (backend, &message, 0);
-                size_t more_size = sizeof (more);
-                zmq_getsockopt (backend, ZMQ_RCVMORE, &more, &more_size);
-                zmq_send (frontend, &message, more? ZMQ_SNDMORE: 0);
-                zmq_msg_close (&message);
-                if (!more)
-                    break; // Last message part
-            }
-        }
-    }
-    // We never get here but clean up anyhow
-    zmq_close (frontend);
-    zmq_close (backend);
-    zmq_term (context);
-    return 0;
-}
 
 def main():
     """ main method """
@@ -84,29 +32,32 @@ def main():
         if (frontend in socks and socks[frontend] == zmq.POLLIN):
             while True:
                 # Process all parts of the message
-                message = []
-                
-                frontend.append(frontend.recv())
+                message = frontend.recv()
                 
                 more = frontend.getsockopt(zmq.RCVMORE)
                 
-                frontend.send(message, more ? zmq.SNDMORE: 0)
+                backend.send(message, zmq.SNDMORE if more else 0)
+                
+                if not more:
+                    break # Last message part
+        if (backend in socks and socks[backend] == zmq.POLLIN):
+            while True:
+                # Process all parts of the message
+               
+                
+                message = backend.recv()
+                
+                more = backend.getsockopt(zmq.RCVMORE)
+                
+                frontend.send(message, zmq.SNDMORE if more else 0)
                 
                 if not more:
                     break # Last message part
 
     # We never get here but clean up anyhow
-    zmq_close (frontend);
-    zmq_close (backend);
-    zmq_term (context);
-    return 0;
-
-
-
-
-
-
-
+    frontend.close()
+    backend.close()
+    context.term()
 
 if __name__ == "__main__":
     main()
