c274b2594ac3f89fe7b5d56f9c128470b7eb8105
aubert <guillaume.aubert@gmail.com>
2011-05-11 22:08:03 +0200

    update side_card.sh

--- etc/img-manip/side_card.sh
+++ etc/img-manip/side_card.sh
@@ -14,7 +14,7 @@ in=$1
 out=$3
 text=$2
 
-usage="./bottom_card.sh input_file \"My text to add\" output_file"
+usage="./side_card.sh input_file \"My text to add\" output_file"
 
 if [ -z "$in" ]; then
   echo "in is not defined"
@@ -43,9 +43,9 @@ rm -f out.jpg
 cd $working_dir
 
 #resize image and border of 25x25
-$convert $in -normalize -resize 640 -bordercolor White -border 25x25 dummy.jpg
+$convert $in -normalize -resize 400 -bordercolor White -border 12x12 dummy.jpg
 # add bigger bottom border
-$convert dummy.jpg -gravity south -splice 0x60 -background White -gravity Center -append temp.jpg
+$convert dummy.jpg -gravity east -splice 300x0 -background White -append temp.jpg
 #get width and height of the produced picture
 W=`convert dummy.jpg -format %w info:`
 #echo "W is $W"
@@ -54,26 +54,27 @@ W=`convert dummy.jpg -format %w info:`
 # create a mask fro rounding the borders
 # apply the mask
 #create mvg mask
-$convert temp.jpg -format 'roundrectangle 1,1 %[fx:w+4],%[fx:h+4] 15,15' info: > rounded_corner.mvg
+$convert temp.jpg -format 'roundrectangle 1,1 %[fx:w+4],%[fx:h+4] 12,12' info: > rounded_corner.mvg
 $convert temp.jpg -border 3 -alpha transparent -background none -fill white -stroke none -strokewidth 0 -draw "@rounded_corner.mvg" rounded_corner_mask.png
 $convert temp.jpg -matte -bordercolor none -border 3 rounded_corner_mask.png -compose DstIn -composite temp.png 
 # add drop shadow
 $convert temp.png \( +clone -background black -shadow 80x3+20+20 \) +swap -background white -layers merge +repage shadow.png 
+cp shadow.png /home/aubert/Dev/projects/rodd/etc/img-manip
 #create label and add it with composite
 
 #$convert -font /homespace/gaubert/.gimp-2.6/plug-ins/fonts/Candice.ttf  -pointsize 36 label:"$text" label.png 
 #$convert -font /homespace/gaubert/.gimp-2.6/plug-ins/fonts/Candice.ttf  -size "$W"x85 -pointsize 36 caption:"$text" label.png
 
 #label size: remove the 50 pixels corresponding to the borders
-W=$(($W-50))
-$convert -font /homespace/gaubert/.gimp-2.6/plug-ins/fonts/Candice.ttf -gravity center -size "$W"x60 label:"$text" label.png
+#W=$(($W-50))
+#$convert -font /homespace/gaubert/.gimp-2.6/plug-ins/fonts/Candice.ttf -gravity center -size "$W"x60 label:"$text" label.png
 #when we have a label without any size
 #$composite label.png -gravity south -geometry +0+52 shadow.png out.png
 
-$composite label.png -gravity south -geometry +0+40 shadow.png out.png
-cp out.png $out
+#$composite label.png -gravity south -geometry +0+40 shadow.png out.png
+#cp out.png $out
 
-rm -Rf $working_dir
+#rm -Rf $working_dir
 
 
 
