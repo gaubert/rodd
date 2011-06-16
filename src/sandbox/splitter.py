44a2c1961d94e90ec09a54cc32803ff28d1080dd
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-06-16 13:51:51 +0000

    start implementing the split

--- src/sandbox/splitter.py
+++ src/sandbox/splitter.py
@@ -10,6 +10,7 @@ import os
 import select
 import fcntl
 import time
+import re
 
 class WMCTRLHelper(object): # pylint: disable-msg=R0903
     """ Python Helper calling WMCTRL executable:
@@ -19,9 +20,12 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
     INFO_PREFIX  = "INFO:"
     DEBUG_PREFIX = "DEBUG:"
     
-    def __init__(self):
+    def __init__(self, buffer_size, select_timeout,base_command):
         """ constructor """
-        pass
+        self._buffer_size     = buffer_size
+        self._select_timeout  = select_timeout
+        self._command         = base_command 
+        self._desktop_dimensions = None
     
     @classmethod
     def _set_flags(cls, a_proc):
@@ -36,12 +40,113 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
         if not a_proc.stderr.closed:
             fcntl.fcntl(a_proc.stderr, fcntl.F_SETFL, flags| os.O_NONBLOCK)
             
-    def active_window_north_east(self, a_timeout = 10800):
+    def _generate_wmctrl_command_line(self, x, y, width, height):
+        """
+          wmctlr -r :ACTIVE: -e 1,x,y,width,height.
+          example "/bin/wmctrl -r :ACTIVE: -e 1,960,580,950,580"
+        """
+        
+        return "%s -r :ACTIVE: -e 1,%s,%s,%s,%s" % (self._command, x, y, width, height)
+        
+    def create_move_to_full_screen_command(self):
+        """ 
+           Move command north_east.
+           -e gravity,x_position,y_position,width,height
+        
+        """
+        x_pos   = 0
+        y_pos   = 0
+        width   = self._desktop_dimensions["width"]
+        height  = self._desktop_dimensions["height"]
+        
+        return self._generate_wmctrl_command_line(x_pos, y_pos, width, height)
+            
+    def create_move_to_window_north_east_command(self):
+        """ 
+           Move command north_east.
+           -e gravity,x_position,y_position,width,height
+        
+        """
+        x_pos   = self._desktop_dimensions["width"] / 2
+        y_pos   = 0
+        width   = self._desktop_dimensions["width"] / 2
+        height  = self._desktop_dimensions["height"] / 2
+        
+        return self._generate_wmctrl_command_line(x_pos, y_pos, width, height)
+    
+    def create_move_to_window_north_west_command(self):
+        """ 
+           Move command north_east.
+           -e gravity,x_position,y_position,width,height
+        
+        """
+        x_pos   = 0
+        y_pos   = 0
+        width   = self._desktop_dimensions["width"] / 2
+        height  = self._desktop_dimensions["height"] / 2
+        
+        return self._generate_wmctrl_command_line(x_pos, y_pos, width, height)
+    
+    def create_move_to_window_east_command(self):
+        """ 
+           Move command north_east.
+           -e gravity,x_position,y_position,width,height
+        
+        """
+        x_pos   = self._desktop_dimensions["width"] / 2
+        y_pos   = 0
+        width   = self._desktop_dimensions["width"] / 2
+        height  = self._desktop_dimensions["height"]
+        
+        return self._generate_wmctrl_command_line(x_pos, y_pos, width, height)
+    
+    def create_move_to_window_west_command(self):
+        """ 
+           Move command north_east.
+           -e gravity,x_position,y_position,width,height
+        
+        """
+        x_pos   = 0
+        y_pos   = 0
+        width   = self._desktop_dimensions["width"] / 2
+        height  = self._desktop_dimensions["height"]
+        
+        return self._generate_wmctrl_command_line(x_pos, y_pos, width, height)
+    
+    
+    def _get_desktop_dimensions(self):
+        """ get the desktop dimensions """
+        command = "xdpyinfo | grep dimension"
+        
+        retval, output = self.execute_command(command)
+        
+        if retval == 1:
+            raise Exception("Error: Cannot get the desktop dimensions. Err= %s" %(output['stderr']))
+        
+        stdout = ''.join(output['stdout'])
+        
+        #strToParse = "  dimensions:    1920x1200 pixels (650x406 millimeters)"
+    
+        pattern ="\s*dimensions:\s*(?P<width>\d*)x(?P<height>\d*)\spixels\s*"
+        
+        reSpec = re.compile(pattern, re.IGNORECASE)
+    
+        m = reSpec.match(stdout)
+    
+        if m is not None:
+            self._desktop_dimensions = {"width" : int(m.group('width')), "height" : int(m.group('height'))}
+            
+        else:
+            raise Exception("Error: Cannot find dimensions in %s" % (stdout))
+        
+            
+    def execute_command(self, a_command, a_timeout = 10800):
         """ send active windows to north east corner """
-        command="/homespace/gaubert/bin/wmctrl -r :ACTIVE: -e 1,960,580,950,580"
+        #information returned to the user
+        user_info = { 'stdout' : [], 'stderr' : []}
         
         #execute process 
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env= self._get_env())
+        proc = subprocess.Popen(a_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env= self._get_env())
         
         WMCTRLHelper._set_flags(proc)
             
@@ -69,7 +174,7 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
                 if src_fd == proc.stdout:
                     # handle the server socket
                     buf = src_fd.read(self._buffer_size)
-                    a_dest_fd.write(buf)
+                    user_info['stdout'].append(buf)
                     #nothing else to read => stdout is closed and the processed is finished
                     if not buf:
                         proc.stdout.close()
@@ -78,7 +183,7 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
                         
                 elif src_fd == proc.stderr:
                     buf = src_fd.read(self._buffer_size)
-                    user_info.append(buf)
+                    user_info['stderr'].append(buf)
                     #nothing else to read => stdout is closed and the processed is finished
                     if not buf:
                         proc.stderr.close()
@@ -88,90 +193,8 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
         # for the moment wait for ever (will have find a way to kill the process if necessary)
         retval = proc.wait()
         
-        self._logger.info("returned value %d" % (retval) )
-        
         return retval, user_info
         
-    def get_wave_data(self, a_dest_fd, a_stations_dict, a_begin, a_end, a_format, a_timeout = 10800):#pylint:disable-msg=R0913,R0914
-        """ Get one option from a section.
-        
-            return the default if it is not found and if fail_if_missing is False, otherwise return NoOptionError
-          
-            :param a_dest_fd: file descriptor where the data is going to be written
-            :param a_stations_dict: A dictionnary containing stations (key) and a list of related channels (value)
-            :type  section: dictionnary
-            :param a_begin: begin date
-            :type  section: date
-            :param a_end: end date
-            :type  section: str
-            :param a_format: returned format (INT | CM6 | CSF | RAW)
-            :type  section: str
-            :param a_timeout: timeout after which this method should come back (default 3hrs (10800 seconds))
-            :type  section: str
- 
-            :returns: the option as a string
-            
-            :except NoOptionError: Raised only when fail_is_missing set to True
-        
-        """
-        
-        #information returned to the user
-        user_info = []
-        
-        command = self._build_command(a_stations_dict, a_begin, a_end, a_format)
-        
-        #execute process 
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env= self._get_env())
-        
-        WMCTRLHelper._set_flags(proc)
-            
-        inputs = [proc.stdout, proc.stderr]
-            
-        reading_on_stdout = True
-        reading_on_stderr = True
-               
-        #time limit in seconds
-        limit = time.time() + a_timeout
-        
-        while reading_on_stdout or reading_on_stderr:
-            
-            # the select will block for up to select_timeout seconds before to come back 
-            inputready, _, _ = select.select(inputs, [], [], self._select_timeout)
-            
-            #if we have reached the time limit for the process quit in error
-            if time.time() > limit:
-                # when using 2.6 call terminate and kill maybe
-                #p.terminate()
-                WaveReaderHelper._kill_process(proc.pid)
-                return (15,"ERROR: The request is still running after %d sec. Abort in error.")
-        
-            for src_fd in inputready:
-                if src_fd == proc.stdout:
-                    # handle the server socket
-                    buf = src_fd.read(self._buffer_size)
-                    a_dest_fd.write(buf)
-                    #nothing else to read => stdout is closed and the processed is finished
-                    if not buf:
-                        proc.stdout.close()
-                        reading_on_stdout = False
-                        inputs.remove(proc.stdout)
-                        
-                elif src_fd == proc.stderr:
-                    buf = src_fd.read(self._buffer_size)
-                    user_info.append(buf)
-                    #nothing else to read => stdout is closed and the processed is finished
-                    if not buf:
-                        proc.stderr.close()
-                        reading_on_stderr = False
-                        inputs.remove(proc.stderr)
-
-        # for the moment wait for ever (will have find a way to kill the process if necessary)
-        retval = proc.wait()
-        
-        self._logger.info("returned value %d" % (retval) )
-        
-        return retval, user_info
-    
     @classmethod
     def _kill_process(cls, a_pid):
         """
@@ -204,10 +227,28 @@ class WMCTRLHelper(object): # pylint: disable-msg=R0903
                    # get LD_LIBRARY_PATH to load the oracle libs
                    "LD_LIBRARY_PATH": os.environ.get('LD_LIBRARY_PATH','ERROR_NOT_SET'),
                   }
+        #return the_env
+        return os.environ
         
-        return the_env
 
 
 
 if __name__ == '__main__':
-    pass
+    
+    
+    helper = WMCTRLHelper(50*1024, 10000,"/homespace/gaubert/bin/wmctrl")
+    
+    helper._get_desktop_dimensions()
+    
+    retval, user_info = helper.execute_command(helper.create_move_to_window_north_east_command())
+    
+    time.sleep(1)
+    
+    retval, user_info = helper.execute_command(helper.create_move_to_window_north_west_command())
+    
+    time.sleep(1)
+    
+    retval, user_info = helper.execute_command(helper.create_move_to_full_screen_command())
+    
+    
+    #print("retval, userinfo = %s, %s\n" % (retval, user_info))
