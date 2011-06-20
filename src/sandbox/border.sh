25cb311e2ff7152d52c56f871d4ade0be350edfc
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-06-20 15:28:32 +0000

    border.sh gives the border size

--- src/sandbox/border.sh
+++ src/sandbox/border.sh
@@ -6,19 +6,13 @@
 #   
 #set -x
 
-WINDOW_IDS=`xwininfo -int -root -tree   |\
-    grep '[0-9]*\ (has no name)' -v     |\
-    grep -Eo '[0-9]{5,}'`
-
-for win in $WINDOW_IDS;
-do 
-    xprop -id $win |\
+xprop -id $1 |\
         grep -Ee '^(_NET_FRAME_EXTENTS|WM_CLASS)' |\
         sed 's/.*=\ //' |\
         sed -e :a -e '/$/N;s/\n/ /;ta' |\
         grep  ^[0-9]    |\
-    while read line;
-    do
+while read line;
+ do
         set -- $line
         E=`echo $1|sed 's/,$//'`
         W=`echo $2|sed 's/,$//'`
@@ -27,5 +21,4 @@ do
         NAME=`echo $5|sed 's/,$//'`
         CLASS=`echo $6|sed 's/,$//'`
         echo -e "$CLASS $NAME N=$N, E=$E S=$S W=$W"
-    done
-done
+ done
