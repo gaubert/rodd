04ee8fc5eb24ddf2a18122f20f3e682ddbf1db97
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-11-16 21:25:34 +0100

    add imap_test with imap lib

--- src/sandbox/imap_test.py
+++ src/sandbox/imap_test.py
@@ -5,11 +5,10 @@ Created on Nov 15, 2011
 '''
 import base64
 from imapclient import IMAPClient
+import imaplib
     
 HOST = 'imap.gmail.com'
 PORT = 993
-USERNAME = ''
-PASSWORD = ''
 ssl = True
 
 def obfuscate_string(a_str):
@@ -28,38 +27,68 @@ def read_password_file(a_path):
     line = f.readline()
     (login, passwd) = line.split(":")
     
-    return (login.strip(), passwd.strip())
+    return (deobfuscate_string(login.strip()), deobfuscate_string(passwd.strip()))
 
 
 
-def imap_test():
-   
+def imaplib_test(login, password):
+    """
+       IMAP client
+    """
+    
+    mail = imaplib.IMAP4_SSL('imap.gmail.com')
+    mail.login(login, password)
+    mail.list()
+    # Out: list of "folders" aka labels in gmail.
+    mail.select("[Gmail]/All Mail") # connect to inbox.
+    
+    result, data = mail.search(None, "ALL")
+    
+    ids = data[0] # data is a list.
+    
+    print("ids = %s\n" %(ids))
+    
+    id_list = ids.split() # ids is a space separated string
+    latest_email_id = id_list[-1] # get the latest
+
+    result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+
+    raw_email = data[0][1] # here's the body, which is raw text of the whole email
+    # including headers and alternate payloads
+    print("hello\n")
+     
+
+def imapclient__test(login, password):
+    
     server = IMAPClient(HOST, use_uid=True, ssl=ssl)
-    server.login(USERNAME, PASSWORD)
+    server.login(login, password)
     
     capabilities = server.capabilities()
     
     print("list folders = %s\n" %(server.list_folders()))
        
-    select_info = server.select_folder('INBOX')
+    select_info = server.select_folder('[Gmail]/All Mail')
     
-    print '%d messages in INBOX' % select_info['EXISTS']
+    #print '%d messages in INBOX' % select_info['EXISTS']
       
     messages = server.search(['NOT DELETED'])
-    print "%d messages that aren't deleted" % len(messages)
+    print "%d messages in [Gmail]/All Mail" % len(messages)
        
-    print
-    print "Messages:"
-    response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE', 'RFC822'])
-    for msgid, data in response.iteritems():
-        print '   ID %d: %d bytes, flags=%s, data=%s' % (msgid, data['RFC822.SIZE'], data['FLAGS'], data['RFC822'])
+    select_info = server.select_folder('[Gmail]/Sent Mail')
+    
+    #print '%d messages in INBOX' % select_info['EXISTS']
+      
+    messages = server.search(['NOT DELETED'])
+    print "%d messages in [Gmail]/Sent Mail" % len(messages)   
+    
+    #response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE', 'RFC822'])
+    #for msgid, data in response.iteritems():
+    #    print '   ID %d: %d bytes, flags=%s, data=%s' % (msgid, data['RFC822.SIZE'], data['FLAGS'], data['RFC822'])
 
 if __name__ == '__main__':
     
     log, pas = read_password_file('/homespace/gaubert/.ssh/passwd')
     
-    print("obfuscated pass: %s\n" % (deobfuscate_string(pas)))
-    print("obfuscated log: %s\n" % (deobfuscate_string(log)))
-    
-    print(read_password_file('/homespace/gaubert/.ssh/passwd'))
+    imaplib_test(log,pas)
+    #imap_test(log, pas)
     
