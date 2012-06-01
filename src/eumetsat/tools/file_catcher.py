994c942b029259fd5a609d7e3e624cd321110f51
Guillaume Aubert <guillaume.aubert@gmail.com>
2012-06-01 14:40:18 +0000

    add euler pb

--- src/eumetsat/tools/file_catcher.py
+++ src/eumetsat/tools/file_catcher.py
@@ -10,6 +10,7 @@ import shutil
 import time
 import fnmatch
 import sys
+import getopt
 
 def makedirs(aPath):
     """ my own version of makedir """
@@ -33,34 +34,80 @@ def find_and_copy_file(patterns, srcs, dest, copied_files_list):
             #print("Check %s in %s" % (the_file, src))
             for pattern in patterns:
                 if fnmatch.fnmatch(the_file, pattern) and not (the_file in copied_files_list):
-                    shutil.copyfile('%s/%s' % (src, the_file), '%s/%s' % (dest, the_file))
-                    print("Copied %s/%s in %s" % (src, the_file, dest))
+                    #shutil.copyfile('%s/%s' % (src, the_file), '%s/%s' % (dest, the_file))
+                    shutil.move('%s/%s' % (src, the_file), '%s/%s' % (dest, the_file))
+                    print("Info: Moved %s/%s in %s" % (src, the_file, dest))
                     files_found = True
                     copied_files_list.append(the_file)
     
     if not files_found:
-        print("Could not find any new files matching %s" % (pattern))
+        print("Info: Could not find any new files matching %s" % (pattern))
 
 
-
-
+def parse_args():
+    """ parse arguments"""
+    
+    options, _ = getopt.getopt(sys.argv[1:], 's:d:hw', ['srcs=', 
+                                                             'dests=',
+                                                             'help',
+                                                             'wait'
+                                                              ])
+    #print 'OPTIONS   :', options
+    help = None
+    wait = False
+    sources = None
+    dest = None
+    
+    for opt, arg in options:
+        if opt in ('-s', '--srcs'):
+            sources = arg
+        elif opt in ('-d', '--dests'):
+            dest = arg
+        elif opt in ('-w', '--wait'):
+            wait = True
+        elif opt in ('-h', '--help'):
+            help = True
+    
+   
+    if help:
+        print("Usage:\n   $>python file_catcher.py -s dir-srcs -d dir-dest\n\n For example:\n   $>python file_catcher.py -s /data/data_share -d /tmp/results")
+        sys.exit(0)
+        
+    if not sources:
+        print("Error: Need sources. See usage with -h")
+        sys.exit(1)
+    
+    if not dest:
+        print("Error: Need sources. See usage with -h")
+        sys.exit(1)
+        
+    
+    sources = sources.split(',')
+    
+    return sources, dest, wait
+       
 
 if __name__ == '__main__':
     
-    if len(sys.argv) != 3:
-        print(" Error: %s needs 2 arguments (and only 2).\n\n Usage:\n   $>python file_catcher.py dir-src dir-dest\n\n For example:\n   $>python file_catcher.py /data/data_share /tmp/results" % (sys.argv[0]))
-        sys.exit(1)
-    else:
-        print("Source directory: %s\nDestination directory: %s\n" % (sys.argv[1], sys.argv[2]))
-        srcs = [sys.argv[1]]
-        dest = sys.argv[2]
-         
+    srcs, dest, wait = parse_args()
+      
     patterns = ['T_HM*']
+    seconds  = 3
     
     makedirs(dest)
     
     copied_files_list = []
-    while True:
-        find_and_copy_file(patterns, srcs, dest, copied_files_list)
-        print("sleep 7 seconds\n")
-        time.sleep(7) #sleep x seconds
+    
+    try:
+        if wait:
+            print("Info: Go into waiting recurrent mode. Look for files every %s seconds\n" % (seconds))
+            while True:
+                find_and_copy_file(patterns, srcs, dest, copied_files_list)
+                print("Info: Sleep %d seconds\n" % (seconds))
+                time.sleep(seconds) #sleep x seconds
+                
+        else:
+            find_and_copy_file(patterns, srcs, dest, copied_files_list) 
+    except Exception, err:
+        print(err)
+        sys.exit(1)
