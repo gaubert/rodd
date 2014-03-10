33c233fa8480c4430b6d8465478fc976deda7fee
Guillaume Aubert <guillaume.aubert@gmail.com>
2014-03-10 14:35:50 +0100

    work on comparison

--- src/eumetsat/dmon/tests/parse_gems_html_file.py
+++ src/eumetsat/dmon/tests/parse_gems_html_file.py
@@ -1,10 +1,10 @@
 '''
 Created on Feb 14, 2014
 
-@author: gaubert
+@author: guillaume aubert
 '''
 import re
-import eumetsat.dmon.gems_feeder as gems_feeder
+#import eumetsat.dmon.gems_feeder as gems_feeder
 
 "GTS headers of outgoing file W_XX-EUMETSAT-Darmstadt,SOUNDING+SATELLITE,METOPB+GRAS_C_EUMP_20140211232028_07278_eps_o_thn_l1.bin: IEGX01 EUMP 112316; IEGX01 EUMP 112318; IEGX01 EUMP 112319;"
 
@@ -22,11 +22,15 @@ def parse_gems_html():
        Parse the Html GEMS daily file
     """
     
-    fd = open("/Users/gaubert/OneDayOfRMDCNOugoingHeaders.htm")
+    #fd = open("/Users/gaubert/OneDayOfRMDCNOugoingHeaders.htm")
     #fd = open("/Users/gaubert/simple_bull.html")
+    #res_fd = open("/Users/gaubert/result.csv", "w+")
+    #res_fd1 = open("/Users/gaubert/result-by-headers.csv", "w+")
+
+    fd      = open("/home/mobaxterm/DWD-Data/OneDayOfRMDCNOugoingHeaders.htm")
+    res_fd  = open("/home/mobaxterm/DWD-Data/result.csv", "w+")
+    res_fd1 = open("/home/mobaxterm/DWD-Data/result-by-headers.csv", "w+")
     
-    res_fd = open("/Users/gaubert/result.csv", "w+")
-    res_fd1 = open("/Users/gaubert/result-by-headers.csv", "w+")
     
     htmlStr = ""
     
@@ -70,13 +74,83 @@ def parse_gems_html():
         line = ""
         for the_list in bull_map[bull]:
             #for elem in the_list:
-            line += "%s," % the_list
+            line += "%s # " % the_list
         
-        res_fd1.write("%s : %s\n" % (bull, line))
+        res_fd1.write("%s / %s\n" % (bull, line))
 
+def compare_EUM_DWD():
+    """
+       Compare EUM DWD Bulletins
+    """
+    the_dir = "/home/mobaxterm/DWD-Data/" 
+    eum_fd = open("%s/result-by-headers.csv" %(the_dir))
+    dwd_fd = open("%s/DCPC_Eumetsat.txt" % (the_dir))
+
+    set_dwd = set()
+    set_eum = set()
+
+    eum_bull_info = {}
+    dwd_bull_info = {}
+    
+    #read DWD Bulletins
+    for line in dwd_fd.readlines():
+        elems = line.split(",")
+        #print("bulletin %s origin %s" % (elems[6], elems[7]))
+        #set_dwd.add("%s:%s" % (elems[6][1:-1],elems[7][1:-1]))
+        #print("%s:%s\n" % (elems[6][1:-1],elems[7][1:-1]))
+        bull_id = "%s" % (elems[6][1:-1]) 
+        
+        set_dwd.add(bull_id)
+        dwd_bull_info[bull_id] = elems[11]  
+
+    #read EUM Bulletins
+    for line in eum_fd.readlines():
+        elems = line.split("/")
+        #print("bulletins = [%s]" % (elems[0].strip()))
+        bull_id = elems[0].strip().split(":")
+
+        if len(bull_id[0]) < 6:
+            print("Warning bull_id is invalid %s. Ignore it !!!\n" % (elems))
+        else:
+            #print("bull_id = %s" % (bull_id))
+            #set_eum.add(elems[0].strip())
+            set_eum.add(bull_id[0])
+            eum_bull_info[bull_id[0]] = elems[1]
+
+    in_dwd_not_in_eum = set_dwd.difference(set_eum)
+    in_eum_not_in_dwd = set_eum.difference(set_dwd)
+
+    print("total in dwd set %d, nb elem in dwd and not in eum: %d" % (len(set_dwd), len(in_dwd_not_in_eum)))
+    print("total in eum set %d, nb elem in eum and not in dwd: %d" % (len(set_eum), len(in_eum_not_in_dwd)))
+
+    #print("Bulletin IDs in EUMETSAT Bulletin outputs and not on the DWD list")
+
+    #print("|  bullID  |        filename")
+    #print("|----------------------------------------")
+
+    #for b_id in sorted(in_eum_not_in_dwd):
+    #    print("|  %s  |  %s" % (b_id, eum_bull_info[b_id].strip()))    
+
+    print("-------------------------------------------------------------------------------------")
+    print("-------------------------------------------------------------------------------------")
+    print("Bulletin IDs in DWD Bulletin outputs and not on the EUMETSAT list")
+
+    print("|  bullID  |        filename")
+    print("|----------------------------------------")
+
+    for b_id in sorted(in_dwd_not_in_eum):
+        # discard all continuations (T2 = E and ii > 1)
+        if (len(b_id) >= 6 and b_id[1] == "E" and int(b_id[4:6]) > 1) or (dwd_bull_info[b_id].startswith("CONTINUATION")):
+           # discard continuation
+           #something to be done
+           continue
+        else:
+           print("|  %s  |  %s" % (b_id, dwd_bull_info[b_id].strip()))
+
+    #print("in_eum_not_in_dwd = %s" % (in_eum_not_in_dwd)) 
     
 
 if __name__ == '__main__':
     
-    parse_gems_html() 
-   
+    #parse_gems_html() 
+    compare_EUM_DWD()
