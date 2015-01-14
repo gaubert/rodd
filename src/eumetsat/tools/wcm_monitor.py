32fd22a2f5559aec7baa2d03619f3a83e8b307f0
Guillaume Aubert <guillaume.aubert@gmail.com>
2015-01-14 22:06:32 +0100

    add sleeping loop. add emails but will need smtp server or sendmail

--- src/eumetsat/tools/wcm_monitor.py
+++ src/eumetsat/tools/wcm_monitor.py
@@ -4,6 +4,8 @@ Created on Jan 14, 2015
 @author: guillaume.aubert@eumetsat.int
 '''
 import os
+import time
+import smtplib
 import datetime
 
 def previous_quater_dt(dt):
@@ -15,10 +17,17 @@ def previous_quater_dt(dt):
 
 class WCMMonitor(object):
 
-    def __init__(self, file_pattern, src_dir):
-        #do nothing
+    def __init__(self, file_pattern, src_dir, sleep_time):
+        """
+
+        :param file_pattern:
+        :param src_dir:
+        :param sleep_time: sleeping time in seconds
+        :return:
+        """
         self._file_pattern = file_pattern
-        self._src_dir = src_dir
+        self._src_dir      = src_dir
+        self._sleep_time   = sleep_time
 
     def generate_date_pattern(self):
         """
@@ -34,12 +43,14 @@ class WCMMonitor(object):
          Return as a datetime the list of 15 min time since midnight.
         """
         result = []
-        curr_dt = the_datetime
+
+        #look at the files from - 15 min to let some time for the generation
+        curr_dt = the_datetime - datetime.timedelta(minutes=15)
 
         first_dt = curr_dt.replace(hour=00, minute=00)
 
         curr_dt = previous_quater_dt(curr_dt)
-        result.append(curr_dt.strftime('%y%m%d%H%M'))
+        #result.append(curr_dt.strftime('%y%m%d%H%M'))
 
         while curr_dt > first_dt:
             curr_dt = previous_quater_dt(curr_dt - datetime.timedelta(minutes=1))
@@ -72,16 +83,54 @@ class WCMMonitor(object):
         for the_date in sorted(dates):
             filename = file_pattern % (the_date)
             if filename not in the_files:
-                print("Error: Missing generation of file %s by Cinesat." % (filename))
+                #print("Error: Missing generation of file %s by Cinesat." % (filename))
                 missing.append(filename)
 
-        print("Error: the following files have been generated in time: %s" % (missing))
+        if missing:
+            print("Error: the following files have been generated in time: %s" % (missing))
+            self.send_email()
+
+    def send_email(self):
+
+        SERVER = "localhost"
+        FROM   = "wcm_service@eumetsat.int"
+        TO     = ["guillaume.aubert@gmail.com"]
+        SUBJECT = "Hello!"
+        TEXT = "This message was sent with Python's smtplib."
+
+        message = """\
+                  From: %s
+                  To: %s
+                  Subject: %s
+
+                  %s
+                  """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+        server = smtplib.SMTP(SERVER)
+        server.sendmail(FROM, TO, message)
+        server.quit()
+
+
+    def run(self):
+        """
+         run every x minutes as defined in the object (see constructor)
+        :return: None
+        """
+
+        while True:
+            self.monit()
+            #wait for ever
+            time.sleep(self._sleep_time)
+            print("Info: Wake up at %s" % (datetime.datetime.now.strftime("%Y-%m-%d %H:%M:%S")))
+
+
+
 
 if __name__ == '__main__':
     #do something
     file_pattern = "myPattern_%s_kkkkk.txt"
     src_dir = "/tmp/test"
+    sleep_time = 900 # 15 min = 900 seconds
 
-    monitor = WCMMonitor(file_pattern, src_dir)
-
-    monitor.monit()
+    monitor = WCMMonitor(file_pattern, src_dir, sleep_time)
+    monitor.run()
