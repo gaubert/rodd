615ca30f2f397cc748d7ea5cb711a19b4c480084
Guillaume Aubert <guillaume.aubert@gmail.com>
2015-01-14 21:38:18 +0100

    send mail

--- src/eumetsat/tools/wcm_monitor.py
+++ src/eumetsat/tools/wcm_monitor.py
@@ -3,7 +3,7 @@ Created on Jan 14, 2015
 
 @author: guillaume.aubert@eumetsat.int
 '''
-
+import os
 import datetime
 
 def previous_quater_dt(dt):
@@ -15,9 +15,10 @@ def previous_quater_dt(dt):
 
 class WCMMonitor(object):
 
-    def __init__(self, file_pattern):
+    def __init__(self, file_pattern, src_dir):
         #do nothing
         self._file_pattern = file_pattern
+        self._src_dir = src_dir
 
     def generate_date_pattern(self):
         """
@@ -28,7 +29,7 @@ class WCMMonitor(object):
         t = (datetime.datetime.now() - datetime.timedelta(minutes=15)).strftime('%y%m%d%H%M')
 
 
-    def get_list_of_time_since_midnight(the_datetime):
+    def get_list_of_time_since_midnight(self, the_datetime):
         """
          Return as a datetime the list of 15 min time since midnight.
         """
@@ -38,31 +39,49 @@ class WCMMonitor(object):
         first_dt = curr_dt.replace(hour=00, minute=00)
 
         curr_dt = previous_quater_dt(curr_dt)
-        result.append(curr_dt)
+        result.append(curr_dt.strftime('%y%m%d%H%M'))
 
         while curr_dt > first_dt:
             curr_dt = previous_quater_dt(curr_dt - datetime.timedelta(minutes=1))
-            print("curr_dt = %s" % curr_dt )
+            #print("curr_dt = %s" % curr_dt )
             result.append(curr_dt.strftime('%y%m%d%H%M'))
 
         print("result = %s\n" % (result))
 
         return result
 
-    def run(self):
+    def monit(self):
         """
         Run the monitoring
         :return: None
         """
         curr_time = datetime.datetime.now()
 
+        missing = []
+
+        #get dir of the current day
+        the_day_dir = curr_time.strftime('%y%m%d')
+
         dates = self.get_list_of_time_since_midnight(curr_time)
 
-        #foreach files look in the dir if the file is here
+        the_dir = "%s/%s" % (self._src_dir, the_day_dir)
+
+        the_files = sorted(os.listdir(the_dir))
+
+        #foreach dates look in the dir if the file is here
+        for the_date in sorted(dates):
+            filename = file_pattern % (the_date)
+            if filename not in the_files:
+                print("Error: Missing generation of file %s by Cinesat." % (filename))
+                missing.append(filename)
+
+        print("Error: the following files have been generated in time: %s" % (missing))
 
 if __name__ == '__main__':
     #do something
-    file_pattern = "myPattern%S_kkkkk"
+    file_pattern = "myPattern_%s_kkkkk.txt"
+    src_dir = "/tmp/test"
 
-    monitor = WCMMonitor(file_pattern)
+    monitor = WCMMonitor(file_pattern, src_dir)
 
+    monitor.monit()
