ab78d81aa7829e8a22771ed6078fdda3c61ca8a9
Guillaume Aubert <guillaume.aubert@gmail.com>
2015-01-15 11:00:10 +0100

    fix path and other left bugs

--- src/eumetsat/tools/wcm_monitor.py
+++ src/eumetsat/tools/wcm_monitor.py
@@ -9,28 +9,27 @@ import smtplib
 import datetime
 
 
-
 def previous_quater_dt(dt):
-    #how many secs have passed this hour
-    nsecs = dt.minute*60+dt.second+dt.microsecond*1e-6
-    delta = nsecs - (nsecs//900)*900
+    # how many secs have passed this hour
+    nsecs = dt.minute * 60 + dt.second + dt.microsecond * 1e-6
+    delta = nsecs - (nsecs // 900) * 900
     #time + number of seconds to quarter hour mark.
     return dt - datetime.timedelta(seconds=delta)
 
-class WCMMonitor(object):
 
+class WCMMonitor(object):
     SERVER = "localhost"
-    FROM   = "wcm_monitor@eumetsat.int"
-    TO     = ["guillaume.aubert@eumetsat.int"]
-    SUBJECT = "Potential errors. Data not received"
+    FROM = "wcm_monitor@eumetsat.int"
+    TO = ["guillaume.aubert@eumetsat.int"]
+    SUBJECT = "Potential errors. Data missing on IPPSVAL"
 
     MSG_TEMPLATE = """\
-                  From: %s
-                  To: %s
-                  Subject: %s
+From: %s
+To: %s
+Subject: %s
 
-                  %s
-                  """
+%s
+"""
 
     def __init__(self, file_pattern, src_dir, sleep_time):
         """
@@ -41,14 +40,14 @@ class WCMMonitor(object):
         :return:
         """
         self._file_pattern = file_pattern
-        self._src_dir      = src_dir
-        self._sleep_time   = sleep_time
+        self._src_dir = src_dir
+        self._sleep_time = sleep_time
 
     def generate_date_pattern(self):
         """
            generate the date pattern depending on the time
         """
-        #get current time
+        # get current time
         datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         t = (datetime.datetime.now() - datetime.timedelta(minutes=15)).strftime('%y%m%d%H%M')
 
@@ -59,7 +58,7 @@ class WCMMonitor(object):
         """
         result = []
 
-        #look at the files from - 15 min to let some time for the generation
+        # look at the files from - 15 min to let some time for the generation
         curr_dt = the_datetime - datetime.timedelta(minutes=15)
 
         first_dt = curr_dt.replace(hour=00, minute=00)
@@ -85,8 +84,8 @@ class WCMMonitor(object):
 
         missings = []
 
-        #get dir of the current day
-        the_day_dir = curr_time.strftime('%y%m%d')
+        # get dir of the current day
+        the_day_dir = curr_time.strftime('%Y-%m-%d')
 
         dates = self.get_list_of_time_since_midnight(curr_time)
 
@@ -110,12 +109,13 @@ class WCMMonitor(object):
         :param errors: errors msgs describing the missing files
         :return: None
         """
-        text = "The following files have not been generated. Please check Cinestsat and datastream on IPPSVAL.\n %s" % ("\n".join(errors))
+        text = "The following files have not been generated. Please check Cinestsat and datastream on IPPSVAL.\n %s" % (
+            "\n".join(errors))
 
         message = WCMMonitor.MSG_TEMPLATE % (WCMMonitor.FROM, ", ".join(WCMMonitor.TO), WCMMonitor.SUBJECT, text)
 
         server = smtplib.SMTP(WCMMonitor.SERVER)
-        server.sendmail(WCMMonitor.FROM, WCMMonitor.TO, WCMMonitor.message)
+        server.sendmail(WCMMonitor.FROM, WCMMonitor.TO, message)
         server.quit()
 
     def send_exception_email(self, err):
@@ -127,13 +127,16 @@ class WCMMonitor(object):
         try:
             text = "ERROR. Unforseen exception %s" % (err)
 
-            message = WCMMonitor.MSG_TEMPLATE % (WCMMonitor.FROM, ", ".join("guillaume.aubert@eumetsat.int"), "Unforseen error on WCM generation Monitor on IPPSVAL", text)
+            message = WCMMonitor.MSG_TEMPLATE % (
+                WCMMonitor.FROM, ", ".join(["guillaume.aubert@eumetsat.int"]), "Unforseen error on WCM generation Monitor on IPPSVAL", text)
 
             server = smtplib.SMTP(WCMMonitor.SERVER)
-            server.sendmail(WCMMonitor.FROM, WCMMonitor.TO, WCMMonitor.message)
+            server.sendmail(WCMMonitor.FROM, WCMMonitor.TO, message)
             server.quit()
         except Exception, email_err:
-            print("%s-Fatal-Error. Cannot send Unforseen exception email. Original error triggering email sending %s\n. Error preventing to send the email" % (datetime.datetime.now.strftime("%Y-%m-%d %H:%M:%S"), err, email_err))
+            print(
+                "%s-Fatal-Error. Cannot send Unforseen exception email. Original error triggering email sending [%s]\n. Error preventing to send the email [%s]." % (
+                    datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), err, email_err))
 
 
     def run(self):
@@ -147,23 +150,28 @@ class WCMMonitor(object):
             try:
                 self.monit()
 
-                err_number = 0 #everything is normal
+                err_number = 0  # everything is normal
             except Exception, err:
                 print("Error. Received error: [%s]" % (err))
-                err_number +=1
-                if err_number > 1:
+                err_number += 1
+                if err_number > 0:
                     self.send_exception_email(err)
-            finally:
-                print("%s-Info: Will now sleep for %s seconds." % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self._sleep_time))
-                time.sleep(self._sleep_time)
-                print("%s-Info: Wake up at %s" % (datetime.datetime.now.strftime("%Y-%m-%d %H:%M:%S"), datetime.datetime.now.strftime("%Y-%m-%d %H:%M:%S")))
+
+            # cannot use finally Python too old
+            #finally:
+
+            print("%s-Info: Will now sleep for %s seconds." % (
+                datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"), self._sleep_time))
+            time.sleep(self._sleep_time)
+            print("%s-Info: Wake up at %s" % (
+                datetime.datetime.now.strftime("%Y-%m-%d %H:%M:%S"), datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")))
 
 
 if __name__ == '__main__':
-    #do something
-    file_pattern    = "MET10_IR108_8bit_%s.csi"
+    # do something
+    file_pattern = "MET10_IR108_8bit_%s.csi"
     src_dir_pattern = "/export/home/ipps/cinesat/ape_db/%s/MET10/IR108/8bit"
-    sleep_time = 900 # 15 min = 900 seconds
+    sleep_time = 900  # 15 min = 900 seconds
 
     monitor = WCMMonitor(file_pattern, src_dir_pattern, sleep_time)
     monitor.run()
