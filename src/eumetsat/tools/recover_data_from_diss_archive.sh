6d45b3d0c3e6072294659455f53968fcfad653cb
Guillaume Aubert <guillaume.aubert@gmail.com>
2016-02-19 13:28:25 +0100

    replace MTSAT2 with recovery for HIMAWARI

--- src/eumetsat/tools/recover_data_from_diss_archive.sh
+++ src/eumetsat/tools/recover_data_from_diss_archive.sh
@@ -45,34 +45,29 @@ echo "Recovering WCM data for $DATE in $DEST_DIR/$DATE-WCM-Recovered."
 #recover data for WCM
 #Get MET10 data
 
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET10 -p 'H-000-MSG3*IR_108*-'"$FDATE"'*'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET10 -p 'H-000-MSG3__-MSG3________-_________-PRO______-'"$FDATE"'*_,H-000-MSG3__-MSG3________-_________-EPI______-'"$FDATE"'*_'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET10 -p 'H-000-MSG3*IR_108*-'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET10 -p 'H-000-MSG3__-MSG3________-_________-PRO______-'"$FDATE"'*_,H-000-MSG3__-MSG3________-_________-EPI______-'"$FDATE"'*_'
 
 #Get MET7 data (Done)
 
 #because 201501120000 is in 2015-01-11
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*___-'"$FDATE"'*-*'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*PRO______-'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*___-'"$FDATE"'*-*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*PRO______-'"$FDATE"'*'
 
 #because 201501130000 is in 2015-01-12
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*___-'"$FDATE"'*-*'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*PRO______-'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*___-'"$FDATE"'*-*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_2/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MET7 -p 'L-000-MTP___-MET7*PRO______-'"$FDATE"'*'
 
 #Get GOES 13 data
 #L-000-MSG3__-GOES13______-03_9_075W-PRO______-201501110400-__
 
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-13 -p 'L-000-MSG3__-GOES13*-'"$FDATE"'*'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-13 -p 'L-000-MSG3__-GOES13______-*-PRO*-'"$FDATE"'*-__'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-13 -p 'L-000-MSG3__-GOES13*-'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-13 -p 'L-000-MSG3__-GOES13______-*-PRO*-'"$FDATE"'*-__'
 
 #Get GOES 15 data
 
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-15 -p 'L-000-MSG3__-GOES15*'"$FDATE"'*'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-15 -p 'L-000-MSG3__-GOES15*'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$PREV_DATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-15 -p 'L-000-MSG3__-GOES15*'"$FDATE"'*'
+#python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/GOES-15 -p 'L-000-MSG3__-GOES15*'"$FDATE"'*'
 
-#Get MTSAT2
-#L-000-MSG3__-MTSAT2______-06_8_145E-PRO______-201501111300-__
-
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MTSAT -p 'L-000-MSG3__-MTSAT2*-'"$FDATE"'*-C_'
-python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_3/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/MTSAT -p 'L-000-MSG3__-MTSAT2______-*-PRO*-'"$FDATE"'*-__'
-
-
+# GET HIMAWARI
+python file_catcher.py -s /drives/y/archive/EUMETSAT_Data_Channel_4/$EUMDATE -d $DEST_DIR/$DATE-WCM-Recovered/HIMAW -p 'IMG_DK*_'"$FDATE"'*.bz2'
